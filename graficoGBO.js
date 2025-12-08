e943ae29e593fd3c43fa582ed8baa3402c30ddbe
claraarigomes <claraarigomes@gmail.com>
2025-12-08 14:01:24 +0000

    GrafigoGBO e Topfilmes.js
    continuacao do graficoGBO e criacao de js para o top de filmes

--- graficoGBO.js
+++ graficoGBO.js
@@ -4,17 +4,18 @@ let canvasHeight, canvasWidth, padding, graphWidth, graphHeight;
 let movies;
 let year = [];
 
-window.onload = function() {
-    // graph general attributes - mudar
-     canvasHeight = 980;
-     canvasWidth = 1280;
-     padding = 60;
-     graphWidth = canvasWidth - padding * 2;
-     graphHeight = canvasHeight - padding * 2;
+window.onload = function () {
+  // graph general attributes - mudar
+  canvasHeight = 980;
+  canvasWidth = 1280;
+  padding = 60;
+  graphWidth = canvasWidth - padding * 2;
+  graphHeight = canvasHeight - padding * 2;
 
-    movies = "dados/dadosFilmes.csv";
+  movies = "dados/dadosFilmes.csv";
 
-    // create a tooltip, so later we make it visible with the data information
+  // create a tooltip, so later we make it visible with the data information
+  /*
     d3.select('main').append("div")
         .style("opacity", '0') // it is hidden
         .attr("class", "tooltip")
@@ -22,37 +23,34 @@ window.onload = function() {
         .style('position', 'absolute')
         .style("border", "solid")
         .style('padding', '2px');
+*/
 
-    // new svg element
-    svg = d3.select('main')
-        .append('svg')
-        .attr('width', canvasWidth)
-        .attr('height', canvasHeight);
+  // new svg element
+  svg = d3
+    .select("main")
+    .append("svg")
+    .attr("width", canvasWidth)
+    .attr("height", canvasHeight);
 
+  //coisas relativas aos dados
+  d3.csv(movies, (d) => {
+    //console.log(d["Gross Box Office"]);
+    return {
+      year: +d.Year, // make this entry a number +d is to make it a number i think
+      gbo: parseFloat(d["Gross Box Office"].replace(",", ".")) //substituo já aqui para depois n ter problemas a somar
+    };
+  }).then(linegraph);
+};
 
-    //coisas dos dadosss
-     d3.csv(movies, d => {
-    //console.log(d.Year);
-        return {
-            year: +d.Year, // make this entry a number +d is to make it a number i think
-           // pop: (d.Popularity) ? +d.Popularity : -1, // for empty values, replace with -1. if there is data, retrieve as numver, if not, as -1
-          //  awards: d.Awards,
-           // genre: d.Subject
-        }
-    }).then(graph); 
-}    
+function linegraph(data) {
+  //quero agrupar o total do GBO por ano https://observablehq.com/@d3/d3-group
 
-function graph (data) {
-
-    //É preciso converter os nossos valores para somar ent tipo 102,78 para 102.78
-    data.forEach(d => {
-        d.GBO = parseFloat(d["Gross Box Office"].replace(",", ".")); //GBO para Gross Box Office
-    });
   
 
-    //Agrupar o total por ano. https://observablehq.com/@d3/d3-group
-    let total = d3.rollups(data, v => d3.sum(v, d => d.GBO), d => d.Year); //v para value??? check no d3
- 
- 
-    //console.log(d.GBO)
+
+
+  // let total = d3.rollups(data, v => d3.sum(v, d => d.GBO), d => d.Year); //v para value??? check no d3
+  //console.log(total);
+  //for each year:
+  //console.log(d.GBO)
 }
