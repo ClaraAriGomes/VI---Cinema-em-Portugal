49f88440584db7a28d548117af27f6e413f98945
claraarigomes <claraarigomes@gmail.com>
2025-12-13 17:29:39 +0000

    grafico gbo
     inicio de limpeza do codigo para adicionar a interatividade

--- graficoGBO.js
+++ graficoGBO.js
@@ -1,77 +1,110 @@
 //ADICIONAR TITULOS AOS EIXOS
-//LIMPAR CODIGO
+//LIMPAR CODIGO XXXXXXXXXXXXXXXXXXXXXXXXXx
 //VER INTERATIVIDADE
-//REDUZIR NUMEROS PARA 80 
+//REDUZIR NUMEROS PARA 80
 //adicionar interatividade com ver os numeros e maybe se clicar top filmes desse ano???
+let svg;
+let canvasHeight, canvasWidth, padding, graphWidth, graphHeight;
+let dadosGBO;
+
 
 window.onload = function () {
-  // set the dimensions and margins of the graph
-  const width = 1200;
-  const height = 500;
-  const marginTop = 20;
-  const marginRight = 100;
-  const marginBottom = 30;
-  const marginLeft = 40;
+  //graph general attributes
+  canvasHeight = 650;
+  canvasWidth = 1000;
+  padding = 100;
+  graphWidth = canvasWidth - padding * 2;
+  graphHeight = canvasHeight - padding * 2;
 
-  // append the svg object to the body of the page
-  const svg = d3
+  //variável pros nossos dados neste gráfico
+  dadosGBO = "/dados/dadosFilmes.csv";
+
+  //pôr o svg do graph no main nesta div especifica
+  svg = d3
     .select("#grafLinhasEspectadores")
     .append("svg")
-    .attr("width", width + marginLeft + marginRight)
-    .attr("height", height + marginTop + marginBottom)
-    .append("g")
-    .attr("transform", `translate(${marginLeft + 20},${marginTop})`);
+    .attr("width", canvasWidth)
+    .attr("height", canvasHeight);
 
   //Read the data
-  d3.csv("/dados/dadosFilmes.csv",
+  d3.csv(dadosGBO, (d) => {
+    return {
+      //Aqui é definir os dados que vamos trabalhar e dar um jeitinho
+      year: +d.Year,
+      gbo: parseFloat(d["Gross Box Office"].replace(",", ".")) / 1000000,
+    };
+  }).then(rollups);
+};
 
-    // When reading the csv, I must format variables:
-    function (d) {
-      return {
-        year: d3.timeParse("%Y")(d.Year), //tenho que dizer que é ano
-        gbo: parseFloat(d["Gross Box Office"].replace(",", ".")),
-      };
-    }
-).then(
-    // Now I can use this dataset:
-    function (data) {
-    let total = d3.rollups(data, v => d3.sum(v, d => d.gbo), d => d.year); 
 
-    const dataset = total
-        .map(d => ({ year: d[0], totalGross: d[1] }))
 
-    // Add X axis --> it is a date format
-      const x = d3
-        .scaleTime()
-        .domain(d3.extent(data, (d) => d.year))
-        .range([0, width-marginRight]);
+//trabalhar mais a partir daqui arrumar codigo
 
-      svg
-        .append("g") // g é um elemento do svg para agrupar elementos
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).ticks(15));
-
-      // Add Y axis
-      const y = d3
-        .scaleLinear()
-        .domain([0, d3.max(dataset, d => d.totalGross)])
-        .range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
-
-      // Add the line
-      svg
-        .append("path")
-        .datum(dataset)
-        .attr("fill", "none")
-        .attr("stroke", "rgba(120, 176, 231, 1)")
-        .attr("stroke-width", 2)
-        .attr(
-          "d",
-          d3
-            .line()
-            .x((d) => x(d.year))
-            .y((d) => y(d.totalGross))
-        );
-    }
+function rollups(data) {
+  //agrupar com rollups o gbo por ano (rollups para somar)
+  let total = d3.rollups(data,(v) => d3.sum(v, (d) => d.gbo),(d) => d.year
   );
+
+  //console.log(total);
+
+  let dataset = total.map((d) => ({ year: d[0], totalGross: d[1]}));
+
+  let chart = svg
+    .append("g")
+    .attr("transform", `translate(${padding}, ${padding})`);
+
+  //Add X axis
+  let x = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, (d) => d.year))
+    .range([0, graphWidth]);
+
+  chart
+    .append("g") // g é um elemento do svg para agrupar elementos
+    .attr("transform", `translate(0, ${graphHeight})`)
+    .call(d3.axisBottom(x).ticks(21).tickFormat(d3.format("d")));
+
+  //texto do Eixo X
+  chart
+    .append("text")
+    .style("text-anchor", "start")
+    .style("fill", "white")
+    .attr("x", graphWidth)
+    .attr("y", graphHeight)
+    .text("YEAR");
+
+  // Add Y axis
+  let y = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d.totalGross)])
+    .range([graphHeight, 0]);
+  chart.append("g").call(d3.axisLeft(y));
+
+  //texto do Eixo Y
+  chart
+    .append("text")
+    .style("text-anchor", "end")
+    .style("fill", "white")
+    .attr("x", 0)
+    .attr("y", 0)
+    .text("GBO em milhões");
+
+
+
+  // Add the line
+  chart
+    .append("path")
+    .datum(dataset)
+    .attr("fill", "none")
+    .attr("stroke", "rgba(120, 176, 231, 1)")
+    .attr("stroke-width", 2)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x((d) => x(d.year))
+        .y((d) => y(d.totalGross))
+    );
 }
+
+function draw_graph() {}
